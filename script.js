f12aa4e9815831e260a40c63896b3b5bb60d0387
Adib Zouiten <adibzouiten2@gmail.com>
2023-06-02 12:18:11 +0100

    RPS ui

--- script.js
+++ script.js
@@ -1,55 +1,20 @@
-// get computers choice
+let userAnswers;
 
-function getComputerChoice() {
-    let random = Math.floor(Math.random() * 3);
-    if(random == 0){
-        return 'rock';
-    }
-    else if(random == 1){
-        return 'paper';
-    }
-    else if(random == 2){
-        return 'scissors';
-    }
-}
+//? button events 
+rock = document.querySelector('#rock');
 
+rock.addEventListener('click', function (e) {
+    userAnswers = 'rock';
+});
 
-// get user choice
+paper=document.querySelector('#paper');
 
-function getUserChoice() {
-    var checkedValue = document.querySelector('.pick:checked').value;
-    return checkedValue;
-}
+paper.addEventListener('click', function (e) {
+    userAnswers = 'paper';
+});
 
+scissors=document.querySelector('#scissors');
 
-// get result
-
-function getResult() {
-    playerSelection = getUserChoice()
-    computerSelection = getComputerChoice() 
-
-    console.log(`the computer selection is ${computerSelection}`);
-    console.log(`the user selection is ${playerSelection}`);
-
-    if(playerSelection == computerSelection){
-        console.log('draw');
-    }
-    else if(playerSelection == 'rock' && computerSelection == 'paper'){
-        console.log('you lost');
-    }
-    else if(playerSelection == 'rock' && computerSelection == 'scissors'){
-        console.log('you win');
-    }
-    else if(playerSelection == 'paper' && computerSelection == 'rock'){
-        console.log('you win');
-    }
-    else if(playerSelection == 'paper' && computerSelection == 'scissors'){
-        console.log('you lost');
-    }
-    else if(playerSelection == 'scissors' && computerSelection == 'rock'){
-        console.log('you lost');
-    }
-    else if(playerSelection == 'scissors' && computerSelection == 'paper'){
-        console.log('you win');
-    }
-}
+scissors.addEventListener('click', function (e) {
+    userAnswers = 'scissors';
+});
